5e4d10f41c24a48ab7682458c14181a5dd599357
duardamessias <dumessiasdu@gmail.com>
2023-11-08 09:00:49 -0300

    salvar imagem no banco (feito)

--- src/PHP/post.php
+++ src/PHP/post.php
@@ -1,21 +1,31 @@
-<?php 
-
+<?php
 include('connect.php');
 
-if(isset($_POST['submit'])){
-    // $data_publicacao = $_POST['data_publicacao'];
-    //$documento = $_POST['documento'];
-    $imagem = $_FILES['imagem'];
-    $texto = $_POST['texto'];
-    $stmt = $conexao->prepare("INSERT INTO post(imagem,texto) 
-    VALUES ('imagem','$texto')");
+// Verifique se um arquivo de imagem foi enviado
+if (isset($_FILES["imagem"]) && $_FILES["imagem"]["error"] === 0) {
+    $nomeArquivo = $_FILES["imagem"]["name"];
+    $imagem = "../img/" . $nomeArquivo;
 
-    if ($stmt->execute()) {
-        echo "Post feito!";
-       
+    // Move o arquivo para o destino
+    if (move_uploaded_file($_FILES["imagem"]["tmp_name"], $imagem)) {
+
+        // Agora você pode inserir as informações no banco de dados
+        if (isset($_POST['submit'])) {
+            $data_publicacao = date("Y-m-d"); // Altere isso para o formato de data desejado
+            $texto = $_POST['texto'];
+
+            // Use declarações preparadas para evitar injeções de SQL
+            $stmt = $conexao->prepare("INSERT INTO post (imagem, texto, data_publicacao) VALUES (?, ?, ?)");
+            $stmt->bind_param("sss", $imagem, $texto, $data_publicacao);
+
+            if ($stmt->execute()) {
+                echo "Post feito!";
+            } else {
+                echo "Erro ao inserir no banco de dados: " . $stmt->error;
+            }
+        }
     } else {
-        echo "campo vazio " . $stmt->error;
+        echo "Erro ao mover o arquivo para o destino.";
     }
 }
-
 ?>
