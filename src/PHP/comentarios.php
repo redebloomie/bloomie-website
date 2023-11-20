caa5768499a221c920c14d901129d4db31acac3d
duardamessias <dumessiasdu@gmail.com>
2023-11-20 13:10:05 -0300

    comentarios + seguir - php (andamento)

---
+++ src/PHP/comentarios.php
@@ -0,0 +1,33 @@
+<?php
+include('connect.php');
+
+if (isset($_POST['comnetar']) && $_POST['comentar'] == "comentar"){
+  $usuario = $_POST['usuario']; esse campo é pra mostra o nome d usuario que ta comentando, não sei como fazer isso
+  $comentario = $_POST['comentario'];
+  $data = date("d/m/Y");
+  $hora = date ("H:i");
+}
+
+  $comentar = "INSERT INTO comentarios (ID_post, nome, data, hora) VALUES ('$ID_post','$usuario','$comentario','$data','$hora')";
+
+  if(mysql_query($comentar)){
+    echo "Comentário enviado com sucesso"; // comentario salvo no banco (espero)
+
+  }
+
+    // mostrar comentarios na publicação 
+    $selecione = mysql_query("SELECT * FROM comentarios WHERE  ID_post = '$ID_post' ");
+    $conta = mysql_num_rows($selecione);
+    
+    if($conta <= 0){
+        echo "sem comentários";
+    }else{
+        while($row = mysql_fetch_array($selecione)){
+            $usuario = $row['usuario']; 
+            $comentario = $row['comentario'];
+            $data = $row['data'];
+            $hora = $row ['hora'];
+        }
+    }
+
+?>
