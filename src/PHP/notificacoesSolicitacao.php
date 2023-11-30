b51f00c08457aeaeadacee5d1e3681023adfa0db
“Winnie <winniestefany303@gmail.com>
2023-11-30 13:30:21 -0300

    add utf

--- src/PHP/notificacoesSolicitacao.php
+++ src/PHP/notificacoesSolicitacao.php
@@ -42,6 +42,7 @@ mysqli_close($conexao);
 
   <title>Notificações Solicitação</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
